7199cf74c4c609f5b8a84831cb037a439f8df624
WIN-6QQMNL2BSO8\Administrator <makoid0104@gmail.com>
2025-08-24 21:33:24 +0900

    jhm

---
+++ dongmani/src/main/java/com/dmni/dongmani/DongmaniApplication.java
@@ -0,0 +1,13 @@
+package com.dmni.dongmani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongmaniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongmaniApplication.class, args);
+    }
+
+}
